2ad3c0530900fc4a3f6e74fef251f025b72835bb
jmontanholi <jonunes9326@hotmail.com.br>
2021-08-17 13:31:36 -0300

    Add button to change quote

--- src/pages/QuotesPage.js
+++ src/pages/QuotesPage.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './QuotesPage.module.css';
 
-const QuotesPage = () => (
-  <div className={style.div}>
-    <h2>
-      <p className={style.quote}>
-        Pure mathematics is, in its way, the poetry of logical ideas.
-      </p>
-      <p>
-        — Albert Einstein, German theoretical physicist
-      </p>
-    </h2>
-  </div>
-);
+const QuotesPage = () => {
+  const [quote, setQuote] = useState('Pure mathematics is, in its way, the poetry of logical ideas.');
+  const [author, setAuthor] = useState('— Albert Einstein, German theoretical physicist');
+  const changeQuote = async () => {
+    const response = await fetch('https://random-math-quote-api.herokuapp.com');
+    const result = await response.json();
+    const data = await result;
+    setQuote(data.quote);
+    setAuthor(data.author);
+  };
+
+  return (
+    <div className={style.div}>
+      <h2>
+        <p className={style.quote}>
+          { quote }
+        </p>
+        <p>
+          -
+          {' '}
+          {author}
+        </p>
+      </h2>
+      <button className={style.button} type="button" onClick={() => { changeQuote(); }}>New Quote</button>
+    </div>
+  );
+};
 
 export default QuotesPage;
