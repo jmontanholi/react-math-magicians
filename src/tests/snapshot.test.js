2ebfb1e2b7d12f1bd1cb04817c7598b572ce2c87
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-18 15:56:08 -0600

    Add snapshot tests for CalculatorPage and QUotePage

--- src/tests/snapshot.test.js
+++ src/tests/snapshot.test.js
@@ -2,6 +2,8 @@ import TestRenderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import CalculatorPage from '../pages/CalculatorPage';
+import HomePage from '../pages/HomePage';
+import QuotePage from '../pages/QuotesPage';
 
 describe('NavBar', () => {
   it('Renders the NavBar', () => {
@@ -20,5 +22,23 @@ describe('Calculator', () => {
       <CalculatorPage />,
     ).toJSON();
     expect(calculator).toMatchSnapshot();
+  });
+});
+
+describe('Homepage', () => {
+  it('Renders the CalculatorPage', () => {
+    const homepage = TestRenderer.create(
+      <HomePage />,
+    ).toJSON();
+    expect(homepage).toMatchSnapshot();
+  });
+});
+
+describe('Quote', () => {
+  it('Renders the QuotePage', () => {
+    const quotepage = TestRenderer.create(
+      <QuotePage />,
+    ).toJSON();
+    expect(quotepage).toMatchSnapshot();
   });
 });
