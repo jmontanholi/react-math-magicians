7c4a00523f561c0a3e5754a7c5451bb778f7fa38
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-18 16:16:30 -0600

    Made tests for Operate file

--- src/tests/logic.test.js
+++ src/tests/logic.test.js
@@ -1,3 +1,60 @@
-it('sum', () => {
-  expect(1 + 1).toBe(2);
+import operate from '../logic/operate';
+
+describe('operate', () => {
+  test('adds two numbers', () => {
+    const result = operate(2, 2, '+');
+
+    expect(result).toBe('4');
+    expect(result).not.toBe('5');
+  });
+
+  test('subtracts a number from a number', () => {
+    const result = operate(10, 5, '-');
+
+    expect(result).toBe('5');
+  });
+
+  describe('multiplies two numbers', () => {
+
+    test('if positive', () => {
+      const result = operate(10, 5, 'x');
+
+      expect(result).toBe('50');
+    });
+
+    test('if negative', () => {
+      const result = operate(10, -5, 'x');
+
+      expect(result).toBe('-50');
+    });
+  });
+
+  describe('divides two numbers', () => {
+
+    test('if positive', () => {
+      const result = operate(10, 5, '÷');
+
+      expect(result).toBe('2');
+    });
+
+    test('if negative', () => {
+      const result = operate(10, -5, '÷');
+
+      expect(result).toBe('-2');
+    });
+
+    test('throws error when dividing by zero', () => {
+      expect(() => operate(10, 0, '÷')).toThrow();
+    });
+  });
+
+  test('returns the remainder of a division', () => {
+    const result = operate(10, 5, '%');
+
+    expect(result).toBe('0');
+  });
+
+  test('throws error when using an unknown operator', () => {
+    expect(() => operate(10, 5, 'b')).toThrow('Unknown operation \'b\'');
+  });
 });
